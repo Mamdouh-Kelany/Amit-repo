1e94cf64bd1b89e9511cb83e72818eff5a8f25f9
Mamdouh.K <mamdouh.kelany84@gmail.com>
2024-10-23 18:34:34 +0300

    @Eng.George 23-10-2024

---
+++ r_u_playing_Banjo.py
@@ -0,0 +1,10 @@
+
+def Banjo(name):
+    if name.lower()[0] == "r":
+        print (name + " is a player")
+    else:
+        print(name + " not a player")
+
+Banjo("Ramy")
+Banjo("ramy")
+Banjo("Ekramy")
